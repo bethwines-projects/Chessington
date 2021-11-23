c844ac2bac0d124216b73cb43175a1e744400093
Hugh Hudson <Hugh.Hudson@softwire.com>
2021-11-23 13:59:23 +0000

    added king movement

--- Chessington.GameEngine/Pieces/King.cs
+++ Chessington.GameEngine/Pieces/King.cs
@@ -10,7 +10,21 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var currentSquare = board.FindPiece(this);
+            List<Square> availableMoves = new List<Square>();
+
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    availableMoves = CheckSquareInBoundsAndAdd(availableMoves,
+                        new Square(currentSquare.Row + j, currentSquare.Col + i));
+                }
+                
+            }
+            
+            availableMoves.RemoveAll(x => x.Col == currentSquare.Col & x.Row == currentSquare.Row);
+            return availableMoves;
         }
     }
 }
