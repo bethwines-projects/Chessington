a09464a6d167bc314c1ba5f3484e4ef36404b686
Hugh Hudson <Hugh.Hudson@softwire.com>
2021-11-23 12:32:34 +0000

    Added bishop diagonal movements

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 
 namespace Chessington.GameEngine.Pieces
 {
@@ -10,7 +11,28 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var currentSquare = board.FindPiece(this);
+            List<Square> availableMoves = new List<Square>();
+
+            for (int i = currentSquare.Col, j = currentSquare.Row; i < 8 & j < 8; j++, i++)
+            {
+                availableMoves.Add(new Square(j,i));
+            }
+            for (int i = currentSquare.Col, j = currentSquare.Row; i >= 0 & j < 8; j++, i--)
+            {
+                availableMoves.Add(new Square(j,i));
+            }
+            for (int i = currentSquare.Col, j = currentSquare.Row; i < 8 & j >= 0; j--, i++)
+            {
+                availableMoves.Add(new Square(j,i));
+            }
+            for (int i = currentSquare.Col, j = currentSquare.Row; i >= 0 & j >= 0; j--, i--)
+            {
+                availableMoves.Add(new Square(j,i));
+            }
+
+            availableMoves.RemoveAll(x => x.Col == currentSquare.Col & x.Row == currentSquare.Row);
+            return availableMoves;
         }
     }
 }
