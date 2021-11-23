f4d7ea461b3566a7b1c4d69d742f1e4e4675620a
Hugh Hudson <Hugh.Hudson@softwire.com>
2021-11-23 13:25:41 +0000

    refactored lateral and diagonal movements into piece

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -11,28 +11,7 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            var currentSquare = board.FindPiece(this);
-            List<Square> availableMoves = new List<Square>();
-
-            for (int i = currentSquare.Col, j = currentSquare.Row; i < 8 & j < 8; j++, i++)
-            {
-                availableMoves.Add(new Square(j,i));
-            }
-            for (int i = currentSquare.Col, j = currentSquare.Row; i >= 0 & j < 8; j++, i--)
-            {
-                availableMoves.Add(new Square(j,i));
-            }
-            for (int i = currentSquare.Col, j = currentSquare.Row; i < 8 & j >= 0; j--, i++)
-            {
-                availableMoves.Add(new Square(j,i));
-            }
-            for (int i = currentSquare.Col, j = currentSquare.Row; i >= 0 & j >= 0; j--, i--)
-            {
-                availableMoves.Add(new Square(j,i));
-            }
-
-            availableMoves.RemoveAll(x => x.Col == currentSquare.Col & x.Row == currentSquare.Row);
-            return availableMoves;
+            return GetDiagonalMoves(board);
         }
     }
 }
--- Chessington.GameEngine/Pieces/Piece.cs
+++ Chessington.GameEngine/Pieces/Piece.cs
@@ -21,9 +21,59 @@ namespace Chessington.GameEngine.Pieces
             board.MovePiece(currentSquare, newSquare);
         }
 
-        private IEnumerable<Square> GetLateralMoves(Board board)
+        public IEnumerable<Square> GetLateralMoves(Board board)
         {
+            var currentSquare = board.FindPiece(this);
+            List<Square> availableMoves = new List<Square>();
+
+            for (int col = currentSquare.Col; col >=0; col--)
+            {
+                availableMoves.Add(new Square(currentSquare.Row,col));
+            }
             
+            for (int col = currentSquare.Col; col < 8; col++)
+            {
+                availableMoves.Add(new Square(currentSquare.Row,col));
+            }
+            
+            for (int row = currentSquare.Row; row < 8; row++)
+            {
+                availableMoves.Add(new Square(row,currentSquare.Col));
+            }
+            
+            for (int row = currentSquare.Row; row >= 0; row--)
+            {
+                availableMoves.Add(new Square(row,currentSquare.Col));
+            }
+
+            availableMoves.RemoveAll(x => x.Col == currentSquare.Col & x.Row == currentSquare.Row);
+            return availableMoves;
+        }
+        
+        public IEnumerable<Square> GetDiagonalMoves(Board board)
+        {
+            var currentSquare = board.FindPiece(this);
+            List<Square> availableMoves = new List<Square>();
+
+            for (int i = currentSquare.Col, j = currentSquare.Row; i < 8 & j < 8; j++, i++)
+            {
+                availableMoves.Add(new Square(j,i));
+            }
+            for (int i = currentSquare.Col, j = currentSquare.Row; i >= 0 & j < 8; j++, i--)
+            {
+                availableMoves.Add(new Square(j,i));
+            }
+            for (int i = currentSquare.Col, j = currentSquare.Row; i < 8 & j >= 0; j--, i++)
+            {
+                availableMoves.Add(new Square(j,i));
+            }
+            for (int i = currentSquare.Col, j = currentSquare.Row; i >= 0 & j >= 0; j--, i--)
+            {
+                availableMoves.Add(new Square(j,i));
+            }
+
+            availableMoves.RemoveAll(x => x.Col == currentSquare.Col & x.Row == currentSquare.Row);
+            return availableMoves;
         }
 
     }
